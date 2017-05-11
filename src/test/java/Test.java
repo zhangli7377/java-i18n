df99880ef31a0c8bb280858b3e80eb0c405dae7b
li.zhang <li.zhang@ptthink.com>
2017-05-11 14:14:16 +0800

    动态增加国际化文件

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,7 @@
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 import java.util.Locale;
 
@@ -16,8 +18,19 @@ public class Test {
 	public static void main(String[] args) {
 		String[] configs = {"applicationContext.xml"};
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(configs);
+		ResourceBundleMessageSource messageSource = (ResourceBundleMessageSource) ctx.getBean("messageSource");
 		System.out.println(ctx.getMessage("greeting.common",null, Locale.US));
 		System.out.println(ctx.getMessage("greeting.common",null, Locale.CHINA));
+		try {
+			System.out.println(ctx.getMessage("greeting2.common",null, Locale.CHINA));
+		} catch (NoSuchMessageException e) {
+			System.out.println("not find greeting2.common");
+		}
+
+		messageSource.setBasenames("i18n/resource","ds/resource");
+		System.out.println(ctx.getMessage("greeting.common",null, Locale.US));
+		System.out.println(ctx.getMessage("greeting.common",null, Locale.CHINA));
+		System.out.println(ctx.getMessage("greeting2.common",null, Locale.CHINA));
 	}
 
 }
